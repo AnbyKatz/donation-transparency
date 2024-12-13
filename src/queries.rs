58c4e7f4ba45d16b2f9e24ed32d3b5c46d0044f7
Anthony Kalaitzis <anthony.kalaitzis@gmail.com>
2024-12-13 20:12:39 +1030

    Add query to get all donations given a party

--- src/queries.rs
+++ src/queries.rs
@@ -15,18 +15,41 @@ pub async fn fetch_all_donars(pool: &PgPool) -> Result<Vec<String>, sqlx::Error>
     sqlx::query_scalar::<_, String>(&query_str).fetch_all(pool).await
 }
 
-pub async fn fetch_donations(pool: &PgPool) -> Result<Vec<Donation>, sqlx::Error> {
-    let query_str = format!(
-        "SELECT 
+pub async fn fetch_all_financial_years(pool: &PgPool) -> Result<Vec<String>, sqlx::Error> {
+    let query_str = format!("SELECT DISTINCT year FROM {DONATION_TABLE}");
+    info!("Running query: {query_str}");
+    sqlx::query_scalar::<_, String>(&query_str).fetch_all(pool).await
+}
+
+pub async fn fetch_donations_from_party(
+    pool: &PgPool,
+    party: &str,
+    year: Option<String>
+) -> Result<Vec<Donation>, sqlx::Error> {
+    let party_query = format!(r#"SELECT id FROM {PARTY_TABLE} WHERE name = '{party}'"#);
+    info!("{party_query}");
+    let party_id = sqlx::query_scalar::<_, i32>(&party_query).fetch_one(pool).await?;
+
+    let mut donation_query = format!(
+        r#"SELECT 
             d.id,
             d.year,
             d.amount,
             p.name AS party_name,
             dn.name AS donar_name
-        FROM {DONATION_TABLE} d
+        FROM {DONATION_TABLE} d      
         JOIN Party p ON d.party_id = p.id
-        JOIN Donar dn ON d.donar_id = dn.id"
+        JOIN Donar dn ON d.donar_id = dn.id
+        WHERE d.party_id = {party_id}          
+    "#
     );
-    info!("Running query: {query_str}");
-    sqlx::query_as::<_, Donation>(&query_str).fetch_all(pool).await
+    match year {
+        Some(x) => {
+            info!("{x}");
+            donation_query += format!(r#" AND year = '{x}'"#).as_str();
+        }
+        None => (),
+    }
+    info!("{donation_query}");
+    sqlx::query_as::<_, Donation>(&donation_query).fetch_all(pool).await
 }
